0b6e783f64054842ad1e2cd797c28191b26aee48
wbggit <jahangir.alom@webguru-development.com>
2026-09-24 13:03:05 +0530

    update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,7 @@ const { protect, adminOnly } = require('../middleware/auth');
 // All routes here are admin only
 router.get('/', protect, adminOnly, getUsers);
 router.get('/:id', protect, adminOnly, getUserById);
+// Update
 router.put('/:id', protect, adminOnly, updateUser);
 router.delete('/:id', protect, adminOnly, deleteUser);
 
